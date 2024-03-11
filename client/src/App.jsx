4108bab673f2fdbe53237f9017990e2fe490e6b3
teshager <teshager2012@gmail.com>
2024-03-11 11:52:58 -0700

    componet and api update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
-
-export default function App() {
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignUp from './pages/SignUp' 
+import Header from "./components/Header"
+export default function App()  {
   return (
-    <h1 className='text-3xl text-red-500 '>app</h1>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-In" element={<SignIn />} />
+        <Route path="/sign-Up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        
+        </Routes>
+    
+    
+    
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,5 @@
+export default function SignIn() {
+    return (
+<h1>sign in</h1>
+    )
+}
