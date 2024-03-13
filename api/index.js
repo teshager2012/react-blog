bce6f661f0cbdecbb8b59d75ee1cb1f0a63c90c9
teshager <teshager2012@gmail.com>
2024-03-13 13:45:16 -0700

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,17 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
-<<<<<<< HEAD
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
+ 
 const app = express();
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(
     () => { console.log('MongoDB is connected'); }
-=======
-const app = express();
-
-mongoose.connect('mongodb+srv://meetteshager:meetmongoose.ifxhcsa.mongodb.net/?retryWrites=true&w=majority&appName=react-blog').then(
-    () => { console.log('MongoDB connected') }
->>>>>>> 2c49e218ca6b7900b7c6994b7845ccc36b72873e
     
 ).catch((err) => {
     console.log(err);
@@ -19,3 +14,6 @@ mongoose.connect('mongodb+srv://meetteshager:meetmongoose.ifxhcsa.mongodb.net/?r
 app.listen(3000, () => {
     console.log('server is running on port 3000!');
 });
+app.use(
+    '/api/user',userRoutes);
+
